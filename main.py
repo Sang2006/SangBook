b5d3244cc98a87e42f8d7662d593444d3d4cbd73
Sang2006 <81185657+Sang2006@users.noreply.github.com>
2022-11-24 00:04:03 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,161 @@
+import os
+import pwinput
+import random
+import string
+
+
+menu = ['Login', 'Logout', 'Exit']
+
+logged_in = False
+
+
+
+Account_crated = False
+
+def menu_display():
+    for x in menu:
+        print(x)
+        
+
+charactors = string.ascii_letters + string.digits + string.punctuation + " "
+
+global password
+
+
+
+print('Welcome to SangBook')
+
+def menus():
+    
+    menu_display()
+    choose = input(f'Please choose one of the above options : ' ) .lower()
+    
+
+
+    if choose == 'login':
+        
+        if os.path.exists('Account_exists.txt'):
+                global Account_crated
+                Account_crated = True
+
+        if os.path.exists('Account_password.txt'):
+            with open('Account_password.txt', 'r') as password:
+                password = str(password.read())                    
+        if os.path.exists('Account_username.txt'):
+            with open('Account_username.txt', 'r') as username:
+                username = str(username.read())
+        
+
+
+        while True:
+            if Account_crated == True:
+                input_username = input('Please enter your username : ')
+                input_password = pwinput.pwinput('Please enter your password : ')
+                
+                
+                
+                if input_username == username and input_password == password:
+                    print(f'Welcome {username.capitalize()}!')
+                    break
+                    
+                else:
+                    print('Please check your username and password again.')
+
+                
+            else:
+                first_username = input('Please choose your username : ')
+                with open('Account_username.txt', 'w') as username:
+                    username.write(str(first_username))
+                generate_password = input('Do you want to generate a reccomended password? (Yes/No) : ').lower()
+                if generate_password == 'yes':
+                      
+                # Password generation
+                    def generator():
+                        try:
+                            password_length = int(input('How many charactors would you like? : '))
+                            if password_length > len(charactors):
+                                new_charactors = password_length*charactors
+                                password = "".join(random.sample(new_charactors, password_length))
+                            else:
+                                password = "".join(random.sample(charactors, password_length))
+                        except ValueError:
+                                print('Please enter only integer values please!')
+                                generator()
+                        except Exception as e:
+                            print('Something went wrong :(')
+                            print(e)
+                        else:
+                            print(f'Your generated password is {password}')
+                            differnt_password = input('Do you want to generate a different password? (Yes/No) : ').lower()
+                            if differnt_password == 'yes':
+                                generator()
+                            else:
+                                print(f'Ok, Your password is : {password}')
+                                global saved_password
+                                saved_password = password
+                                with open('Account_password.txt', 'w') as password:
+                                    password.write(str(saved_password))
+                                with open('Account_exists.txt', 'w') as exists:
+                                    exists.write(str('Account does exist'))
+                                print('Ok! You have successfully created your account!')
+                                menus()
+                    generator()
+                    
+                # user input password
+                
+                
+                else:
+                    first_password = pwinput.pwinput('Please choose your passwor : ')
+                    with open('Account_password.txt', 'w') as password: 
+                        password.write(str(first_password))
+                    with open('Account_exists.txt', 'w') as exists:
+                        exists.write(str('Account does exist'))
+                    
+                    print('Ok! You have successfully created your account!')
+                    menus()
+    
+    elif choose == 'logout':
+        while True:
+            if os.path.exists('Account_exists.txt'):
+                with open('Account_password.txt', 'r') as verify_password:
+                    verify_password = str(verify_password.read())
+                verify = pwinput.pwinput('Please enter your account password : ')
+
+                
+                
+                if verify == verify_password:
+                    confirm = input('Are you sure you want to Logout? (Yes/No) : ').lower()
+                    
+                    
+                    if confirm == 'yes':
+                        while True:
+                            
+                            double_confirm = input('Please type "Confirm" to logout : ')
+                            if double_confirm == 'Confirm':
+                                
+                                os.remove('Account_exists.txt')
+                                os.remove('Account_password.txt')
+                                os.remove('Account_username.txt')
+                                print('You have successfully logged out!')
+                                menus()
+                                break
+                    else:
+                        print("Ok! You won't be logged off!")
+                        menus()
+                
+                
+                else:
+                    print('Please check your password and try again')
+            
+            
+            else:
+                print('You have already logged out!')
+                menus()
+    
+    else:
+        exit()
+            
+            
+        
+        
+menus()
---
+++ password_generator.py
@@ -0,0 +1,52 @@
+import random
+import string
+
+
+
+charactors = string.ascii_letters + string.digits + string.punctuation + " "
+
+global password
+
+
+
+def generator():
+    try:
+        password_length = int(input('How many charactors would you like? : '))
+        if password_length > len(charactors):
+            new_charactors = password_length*charactors
+            password = "".join(random.sample(new_charactors, password_length))
+        else:
+            password = "".join(random.sample(charactors, password_length))
+    except ValueError:
+            print('Please enter only integer values please!')
+            generator()
+    except Exception as e:
+        print('Something went wrong :(')
+        print(e)
+    else:
+        print(f'Your generated password is {password}')
+        differnt_password = input('Do you want to generate a different password? (Yes/No) : ').lower()
+        if differnt_password == 'yes':
+            generator()
+        else:
+            print(f'Ok, Your password is : {password}')
+            saved_password = password
+
+generator()
+
+
+    
+    
+    
+
+
+
+
+
+
+
+
+# password_length = int(input('How many charactors would you like? : '))
+# password = "".join(random.sample(charactors, password_length))
+# print(password)
+
